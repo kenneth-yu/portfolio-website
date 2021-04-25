3e48067a0e199585b23218e5cad30793d9f13a2a
Yu, Kenneth <Kenneth.Yu@priceline.com>
2021-04-25 19:24:40 -0400

    fixes license key bug

--- src/containers/fullpage.js
+++ src/containers/fullpage.js
@@ -2,7 +2,8 @@ import ReactFullpage from '@fullpage/react-fullpage';
 import Hero from '../components/hero';
 import Wip from '../components/wip';
 
-const anchors = ["Introduction", "Work in Progress", "thirdPage"];
+
+const anchors = ["Introduction", "Work-in-Progress", "thirdPage"];
 
 const Fullpage = () => (
   <ReactFullpage
@@ -10,10 +11,11 @@ const Fullpage = () => (
     anchors={anchors}
     navigation
     navigationTooltips={anchors}
-    licenseKey = {'process.env.FULL_PAGE_LICENSE_KEY'}
+    licenseKey = {process.env.FULL_PAGE_LICENSE_KEY}
     scrollingSpeed = {1000} /* Options here */
 
     render={({ state, fullpageApi }) => {
+      console.log(process.env)
       return (
         <ReactFullpage.Wrapper>
           <div className="section">
