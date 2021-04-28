ebd5ee905973dbdc7bd9b6730d75728746a966c8
Yu, Kenneth <Kenneth.Yu@priceline.com>
2021-04-28 16:15:30 -0400

    adds email to cop

--- src/components/contact.js
+++ src/components/contact.js
@@ -8,7 +8,7 @@ function Contact() {
           <Col style={{textAlign: "center"}} sm={6} >
             <h1 className="headline-text">Contact Me</h1>
             <p style={{paddingBottom: 20}}>Although I am not actively searching for new opportunities, I am always open for suggestions. 
-                Feel free to send me a message and I will get back to you as soon as possible. Thanks for visiting my website!
+                Feel free to send me a message using the button below or by messaging me at mail@kennethyu.dev. <br/> I will try my best to get back to you as soon as possible. Thanks for visiting my website!
             </p>
             <Button style={{padding:"15px 40px 15px 40px"}} href={`mailto:mail@kennethyu.dev`} variant="outline-light">Email Me</Button>{' '}
           </Col>
